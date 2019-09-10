d59ef6f48ac8a947bc86048891c378ffc552d45e
psurendra <jkcboys@gmail.com>
2019-09-10 15:06:45 +0530

    Create Sample.sh

---
+++ Sample.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "file is working......"
+
+# if[ $# -gt 0 ]
+#then
+# if[ -f $1 ]
+#       then
+#               cat $1
+#       else
+#               echo "file not found"$1
+#               fi
+#               else
+#                       echo "Not enough Commandline"
+#fi
