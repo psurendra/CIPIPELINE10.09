2e3276fe25b680933e5c928cb732991d8368d4c4
psurendra <jkcboys@gmail.com>
2019-09-10 15:21:35 +0530

    Update Sample.sh

--- Sample.sh
+++ Sample.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-echo "file is working......"
-
-# if[ $# -gt 0 ]
-#then
-# if[ -f $1 ]
-#       then
-#               cat $1
-#       else
-#               echo "file not found"$1
-#               fi
-#               else
-#                       echo "Not enough Commandline"
-#fi
+#!/bin/bash
+if [ $# -gt 0 ]
+then
+ if [ -f $1 ]
+ then
+  cat $1
+ else
+  echo "File not found"$1
+ fi
+else
+echo "Not enough Command line "
+fi
